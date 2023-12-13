c7d5ab9012f12aec4f6fa77c3b1863ebabe94c2d
Oz-Type <153376108+Oz-Type@users.noreply.github.com>
2023-12-13 11:29:48 +0100

    Update index.js

--- src/gatsby-theme-carbon/components/Header/index.js
+++ src/gatsby-theme-carbon/components/Header/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from 'gatsby-theme-carbon/src/components/Header';
 
-const CustomSwitcher = () => (
+const Switcher = () => (
   <Header>
     IBM&nbsp;<span>Design Language</span>
   </Header>
 );
 
-export default CustomSwitcher;
+export default Switcher;
