b3ab9c0fa498cabba0fb55d9e49be176bec3c88f
Oz-Type <153376108+Oz-Type@users.noreply.github.com>
2023-12-13 12:01:04 +0100

    Update index.js

--- src/gatsby-theme-carbon/components/Header/index.js
+++ src/gatsby-theme-carbon/components/Header/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Header from 'gatsby-theme-carbon/src/components/Header';
 
+const Switcher = () => (
+  <Header>
+   <span>OZTYPE</span>
+  </Header>
+);
 // Remove the Switcher component
 
+export default Switcher;
 export default Header;
